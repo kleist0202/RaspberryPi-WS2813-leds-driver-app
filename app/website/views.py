ac7443fc2017ba0284d47a1bee1de334f1146b70
Przemysław Kleist <pk0202@protonmail.com>
2022-11-16 21:59:44 +0100

    leds turn up automatically after turning on RPI (and leds power supply)

--- app/website/views.py
+++ app/website/views.py
@@ -4,6 +4,7 @@ import multiprocessing
 import psutil
 import board
 from .spi import RgbStrip
+import time
 
 views = Blueprint("views", __name__)
 spi = board.SPI()
@@ -14,14 +15,21 @@ def task(eh):
         #print(f'Running task {threading.current_thread().name}')
         rs.rainbow_cycle(float(eh))
 
-def task_runner(var=None):
+def steady_color(eh):
+    while True:
+        #print(f'Running task {threading.current_thread().name}')
+        rs.set_color(*eh)
+        time.sleep(3)
+
+def task_runner(func=None, var=None):
     processes = psutil.Process().children()
     for p in processes:
         p.kill()
     if var is not None:
-        process = multiprocessing.Process(target=task, args=(var,))
+        process = multiprocessing.Process(target=func, args=(var,))
         process.start()
 
+task_runner(steady_color, (100, 100, 100))
 
 @views.route("/", methods=["GET", "POST"])
 def home():
@@ -31,16 +39,14 @@ def home():
     if request.method == "POST":
         current_mode = request.form.get('modes')
         rainbow_speed = request.form.get('rainbow_speed')
-        #print(request.form)
 
         if 'modes' in request.form:
             if current_mode == '1' or current_mode == '2':
-                task_runner()
                 r = int(request.form.get('color_r'))
                 g = int(request.form.get('color_g'))
                 b = int(request.form.get('color_b'))
-                rs.set_color(r, g, b)
+                task_runner(steady_color, (r, g, b))
             elif current_mode == '3': 
-                task_runner(rainbow_speed)
+                task_runner(task, rainbow_speed)
 
     return render_template("home.html")
